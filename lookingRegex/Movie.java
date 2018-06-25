600a9bbbbb79f2951dd2b020d24832d7fdc6a2c8
Soolomon <kkiirrkkllaanndd@gmail.com>
2018-06-25 22:02:27 +0300

    Add files via upload

---
+++ lookingRegex/Movie.java
@@ -0,0 +1,105 @@
+package com.softserve.lookingRegex;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Movie {
+	public String tittle;
+	private  int rank = 0;
+	private String fInfo;
+
+	Movie(String fInfo) {
+		this.fInfo = fInfo;
+		Matcher takeTittle = Pattern.compile("(?<=Tittle:).*?(?=;)").matcher(fInfo);
+		while (takeTittle.find()) {
+			tittle = fInfo.substring(takeTittle.start(), takeTittle.end());
+		}
+	}
+
+	public static void calcPoint(User user) {
+		for (int a = 0; a < user.getLikeMovie().size(); a++) {
+			for (int b = 0; b < fMovie(user.getLikeMovie().get(a).fInfo).split(",").length; b++) {
+				for (int c = 0; c < Source.getSource().size(); c++) {
+					if (Pattern.compile(fMovie(user.getLikeMovie().get(a).fInfo).split(",")[b])
+							.matcher(fMovie(Source.getSource().get(c).fInfo)).find()) {
+						Source.getSource().get(c).rank++;
+					}
+				}
+			}
+		}
+	}
+
+	private static String fMovie(String film) {
+		String dummy = null;
+		Matcher findActors = Pattern.compile("(?<=Actors:).*?(?=; Des)").matcher(film);
+		while (findActors.find()) {
+			dummy = film.substring(findActors.start(), findActors.end()).replaceAll(";", "")
+					.replaceAll("Genre:|Studious:|Directors:", ",").replaceAll(" +", "").toLowerCase();
+		}
+		return dummy;
+	}
+
+	private static void calcRDF(User user) {
+		int count_same_words = 0;
+		for (int i = 0; i < user.getLikeMovie().size(); i++) {
+			for (int j = 0; j < Source.getSource().size(); j++) {
+				for (Map.Entry<String, Integer> step_user : user.getLikeMovie().get(i).frequencyWords().entrySet())
+					for (Map.Entry<String, Integer> step_next : Source.getSource().get(j).frequencyWords().entrySet())
+						if (step_user.getKey().equals(step_next.getKey()))
+							count_same_words++;
+				Source.getSource().get(j).rank += count_same_words * 2;
+			}
+		}
+	}
+
+	// Finds frequency words in line
+	private Map<String, Integer> frequencyWords() {
+		Map<String, Integer> frequency = new HashMap<>();
+		String s = null; // canonization
+		Matcher Description = Pattern.compile("(?<=Description:).*?(?=;)").matcher(fInfo);
+		while (Description.find()) {
+			s = fInfo.substring(Description.start(), Description.end()).toLowerCase().replaceAll("[,.?:;\"!]", "");
+		}
+
+		String[] words = s.split(" ");
+
+		for (int i = 0; i < words.length; i++) {
+			if (words[i].length() > 3) {
+				if (frequency.containsKey(words[i])) {
+					frequency.put(words[i], (frequency.get(words[i]) + 1));
+				} else {
+					frequency.put(words[i], 1);
+				}
+			}
+		}
+		Map<String, Integer> frequency1 = new HashMap<>();
+		for (Map.Entry<String, Integer> step_user : frequency.entrySet())
+			if (frequency.get(step_user.getKey()) > 2) {
+				frequency1.put(step_user.getKey(), step_user.getValue());
+			}
+		return frequency1;
+	}
+
+	public static void pickUpFilms(User user) {
+		calcPoint(user);
+		calcRDF(user);
+		Source.getSource().sort((a, b) -> b.rank - a.rank);
+	}
+
+	public static void addDependenciesPoint(String genre) {
+		for (int i = 0; i < genre.split(" ").length; i++) {
+			for (int j = 0; j < Source.getSource().size(); j++) {
+				if (Pattern.compile(genre.split(" ")[i]).matcher(Source.getSource().get(j).fInfo).find())
+					Source.getSource().get(j).rank++;
+			}
+		}
+	}
+
+	@Override
+	public String toString() {
+		return "Movie [tittle=" + tittle + ", rank=" + rank + "]";
+	}
+
+}
---
+++ lookingRegex/User.java
@@ -0,0 +1,79 @@
+package com.softserve.lookingRegex;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+	private String name;
+	private int age = 1;
+	private boolean sex = true;
+	private List<Movie> likeMovie = new ArrayList<>();
+
+	User(String name, int age, boolean sex) {
+		this.name = name;
+		this.age = age;
+		this.sex = sex;
+		if (age < 12) {
+			Movie.addDependenciesPoint("Fantasy Comedy Anime");
+		} else if (age > 12 && age < 18) {
+			Movie.addDependenciesPoint("Fantasy Comedy Serial");
+		} else if (age > 18 && age < 25) {
+			Movie.addDependenciesPoint("Fantasy Action Adventure");
+		} else {
+			Movie.addDependenciesPoint("Biographical Documentary Historical");
+		}
+		if (sex) {
+			Movie.addDependenciesPoint("Horror War Fantasy Western Fighter");
+		} else {
+			Movie.addDependenciesPoint("Melodram Epics Drama Adventure Romance");
+		}
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public boolean isSex() {
+		return sex;
+	}
+
+	public void setSex(boolean sex) {
+		this.sex = sex;
+	}
+
+	public List<Movie> getLikeMovie() {
+		return likeMovie;
+	}
+
+	public void choise() throws IOException {
+		int choise;
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Please take choise "+this.name+"!");
+		for(int h=0;h<3;h++) {
+		for(int i=0;i<Source.getSource().size();i++) {
+			System.out.println("#"+i+" "+Source.getSource().get(i).tittle);
+		}
+			choise = Integer.parseInt(reader.readLine());
+		likeMovie.add(Source.getSource().get(choise));
+		Source.getSource().remove(choise);
+		
+		}
+
+	}
+
+}
